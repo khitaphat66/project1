976d908481a43f326b5db5bf1974e55097b8bcb0
khitaphat66 <115603191+khitaphat66@users.noreply.github.com>
2025-05-13 12:35:32 +0700

    Update eye_contect.py

--- eye_contect.py
+++ eye_contect.py
@@ -1,31 +1,47 @@
+from picamera2 import Picamera2
+from libcamera import Transform
 import cv2
+import time
 
 # โหลดโมเดล Haar cascade สำหรับใบหน้าและดวงตา
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('/home/user/haarcascades/haarcascade_eye.xml')
+face_ascade = cv2.CascadeClassifier("/home/user/haarcascades/haarcascade_frontalface_default.xml")
 
-# เปิดกล้อง (0 หมายถึงกล้องเว็บแคมตัวหลัก)
-cap = cv2.VideoCapture(0)
+picam2 = Picamera2()
+#config = picam2.create_preview_configuration(main={"size": (640, 480)})
+config = picam2.create_preview_configuration(
+    main={"size": (640, 480), "format": "RGB888"},
+    transform=Transform(hflip=0, vflip=0)
+    )
+picam2.configure(config)
+picam2.start()
+
+time.sleep(2)
+picam2.set_controls({"AwbEnable": True})
+
+def is_valid_eye(w,h):
+    aspect_ratio = w/h
+    return 0.5 < aspect_ratio < 2.5 and 10 < w < 150 and 10 < h < 150
 
 while True:
-    ret, frame = cap.read()  # อ่านภาพจากกล้อง
+    frame = picam2.capture_array("main")
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # แปลงภาพเป็น grayscale
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # ตรวจจับใบหน้า
-    for (x, y, w, h) in faces:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)  # ตรวจจับดวงตา
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)  # วาดกรอบรอบตา
+    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)  # ตรวจจับดวงตา
+    
+    for (x, y, w, h) in eyes:
+        if is_valid_eye(w,h):
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)  # วาดกรอบรอบตา
+        else:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)  # วาดกรอบรอบตา
 
     cv2.imshow('Eye Detection', frame)  # แสดงผลภาพ
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # กด 'q' เพื่อออก
         break
 
-cap.release()
 cv2.destroyAllWindows()
+
+
 
 ---------------------------------------------------------
 from picamera2 import Picamera2
